e525e0ba2f7049877231e73c1bb6ee24e88ffe64
tvcou <tvcou.zyx77@gmail.com>
2016-01-23 22:47:17 +0000

    "add no room_category ok vi app/models/hotel.rb"

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,6 +1,7 @@
 class Hotel < ActiveRecord::Base
   has_many :room_categories, dependent: :destroy
-  accepts_nested_attributes_for :room_categories
+  accepts_nested_attributes_for :room_categories,
+                                reject_if: proc { |attributes| attributes['name'].blank? }
 
   validates :name, presence: true
 
